e73a64edd3c8404cba45db2a72dd986e8ade9994
Poisy <atanas123atanasov@gmail.com>
2020-09-25 19:31:01 +0300

    first added, added feature for searching online for books

---
+++ POILibrary/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POILibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string PagesCont { get; set; }
+        public List<string> Authors { get; set; }
+        public string YearReleased { get; set; }
+        public DateTime YearReaded { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
---
+++ POILibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POILibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books =  LibraryManager.ScrapBooks("bakemonogatari");
+        }
+    }
+}
